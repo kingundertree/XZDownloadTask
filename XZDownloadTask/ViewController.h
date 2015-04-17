d3d1792130465978de89896071ad0b4432803a5f
shanxu <kingundertree@163.com>
2015-04-17 18:09:34 +0800

    add pro

---
+++ XZDownloadTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZDownloadTask
+//
+//  Created by xiazer on 15/4/17.
+//  Copyright (c) 2015年 anjuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XZDownloadTaskTests/XZDownloadTaskTests.m
@@ -0,0 +1,40 @@
+//
+//  XZDownloadTaskTests.m
+//  XZDownloadTaskTests
+//
+//  Created by xiazer on 15/4/17.
+//  Copyright (c) 2015年 anjuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface XZDownloadTaskTests : XCTestCase
+
+@end
+
+@implementation XZDownloadTaskTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
